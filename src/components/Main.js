ab0507d6d074b4522afe2009191ab2bec950c4f6
Nick Liu <imjustanemail@gmail.com>
2016-07-13 09:44:15 +0800

    update packages version

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,5 @@
 require('normalize.css/normalize.css')
-require('styles/App.css')
+import style from 'styles/App.css'
 import { Link } from 'react-router'
 
 import React from 'react'
@@ -9,9 +9,9 @@ let yeomanImage = require('../images/yeoman.png')
 class AppComponent extends React.Component {
   render() {
     return (
-      <div className="index">
+      <div className={style.index}>
         <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
+        <div className={style.notice}>Please edit <code>src/components/Main.js</code> to get started!</div>
         <Link to="/demo">Demo</Link>
       </div>
     )
